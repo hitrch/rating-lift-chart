f38b01b70df90bcbc1e128623ed98f60d90fda20
hitrch <vladislawpaliy@gmail.com>
2021-07-20 16:27:08 +0300

    rating lift chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RatingLiftChartComponent } from './rating-lift-chart/rating-lift-chart.component';
+import {ToggleButtonModule} from 'primeng/togglebutton';
+import {FormsModule} from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RatingLiftChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToggleButtonModule,
+    FormsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
